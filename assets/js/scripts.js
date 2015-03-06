bd73596c168eb47f504087a675625bf901203a66
emanuszak <ellen.manuszak@gmail.com>
2015-03-05 23:28:19 -0500

    Fixed mobile form view

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,18 @@
 $("a[href^='http://']").attr("target","_blank");
 
+
 // Hide and Reveal Contact Form
 
-$(".contactform form").hide();
-$(".contactform").append("<button>Contact Me</button>");
+$("#contactform form").hide();
+$("#contactform").append("<button>Contact Me</button>");
 $("button").addClass("contactme").click(function(){
 	$(this).prev().show();
-	$(".contactform > button").hide();
+	$("#contactform > button").hide();
 });
 
 $("button[type=reset]").click(function(){
-	$(".contactform form").hide();
-	$(".contactform > button").show();
+	$("#contactform form").hide();
+	$("#contactform > button").show();
 });
 
 
